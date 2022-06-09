398eb36d32089e52f7bbd0930825e5169e9334f3
Andrey Sakharov <sakharov.a@n-t.io>
2022-06-09 15:36:19 +0300

    editorconfig

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,23 +13,23 @@ dotenv.config();
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.7',
-  defaultNetwork: 'hardhat',
-  networks: {
-    hardhat: {
-      // the url is 'http://localhost:8545' but you should not define it
-    },
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_ID}`,
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY as string],
-    },
-  },
-  namedAccounts: {
-    deployer: 0,
-    user1: 1,
-    user2: 2,
-    user3: 3,
-  },
+	solidity: '0.8.7',
+	defaultNetwork: 'hardhat',
+	networks: {
+		hardhat: {
+			// the url is 'http://localhost:8545' but you should not define it
+		},
+		rinkeby: {
+			url: `https://rinkeby.infura.io/v3/${process.env.INFURA_ID}`,
+			accounts: [process.env.DEPLOYER_PRIVATE_KEY as string],
+		},
+	},
+	namedAccounts: {
+		deployer: 0,
+		user1: 1,
+		user2: 2,
+		user3: 3,
+	},
 };
 
 export default config;
